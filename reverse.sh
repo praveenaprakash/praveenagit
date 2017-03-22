983f175e58bd7672209e02d0b508488a01a26305
Praveena <praveenaprakash@gamil.com>
2017-03-23 00:09:36 +0530

    reverse added

--- reverse.sh
+++ reverse.sh
@@ -1,3 +1,14 @@
-echo "enter a name"
-read a
- echo $a |rev
+echo "enter a string"
+read str
+len=`echo $str |wc -c`
+len=`expr $len - 1`
+echo " length of the string is ="$len
+reverse=" "
+while [ $len -gt 0 ]
+do
+reverse1=`echo $str |cut -c$len`
+reverse=$reverse$reverse1
+len=`expr $len - 1`
+done
+echo $reverse
+
